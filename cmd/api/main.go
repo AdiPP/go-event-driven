d7e101a303db8d1510b3185e1475b903111d700d
Adi Putra <adiputrapermana@gmail.com>
2024-05-29 15:20:11 +0700

    enhance: add queue and response writer and enhance memory queue adapter

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
+	"reflect"
 
 	"github.com/AdiPP/go-event-driven/internal/application/controller"
 	"github.com/AdiPP/go-event-driven/internal/application/usecase"
+	"github.com/AdiPP/go-event-driven/internal/domain/event"
 	"github.com/AdiPP/go-event-driven/internal/infra/queue"
 )
 
 func main() {
+	ctx := context.Background()
+
+	// initialize queue
 	queueAdapter := queue.NewMemoryQueueAdapter()
 	
+	// use cases
 	createOrderUseCase := usecase.NewCreateOrderUseCase(queueAdapter)
 	processOrderPaymentUseCase := usecase.NewProcessOrderPaymentUseCase(queueAdapter)
 	stockMovementUseCase := usecase.NewStockMovementUseCase()
 	sendOrderEmailUseCase := usecase.NewSendOrderEmailUseCase()
 	
+	// controllers
 	orderController := controller.NewOrderController(
 		createOrderUseCase,
 		processOrderPaymentUseCase,
@@ -24,12 +33,43 @@ func main() {
 		sendOrderEmailUseCase,
 	)
 
+	// register routes
 	http.HandleFunc("POST /create-order", orderController.CreateOrder)
 
-	fmt.Println("Server is running on port 8080")
-	err := http.ListenAndServe(":8080", nil)
+	// mapping listeners
+	var list map[reflect.Type][]func (w http.ResponseWriter, r *http.Request) = map[reflect.Type][] func (w http.ResponseWriter, r *http.Request)  {
+		reflect.TypeOf(event.OrderCreatedEvent{}): {
+			orderController.ProcessOrderPayment,
+			orderController.StockMovement,
+			orderController.SendOrderEmail,
+		},
+	}
+
+	// register listeners
+	for eventType, handlers := range list {
+		for _, handler := range handlers {
+			queueAdapter.ListenerRegister(eventType, handler)
+		}
+	}
+
+	// connect queue
+	err := queueAdapter.Connect(ctx)
 
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error connect queue %s", err)
 	}
+
+	// start consuming queues
+	orderCreatedEvent := reflect.TypeOf(event.OrderCreatedEvent{}).Name()
+
+	go func (ctx context.Context, queueName string)  {
+		queueAdapter.StartConsuming(ctx, queueName)
+
+		if err != nil {
+			log.Fatalf("Error running consumer %s: %s", queueName, err)
+		}
+	}(ctx, orderCreatedEvent)
+
+	fmt.Println("Server is running on port 8080")
+	http.ListenAndServe(":8080", nil)
 }
--- internal/infra/queue/memory_queue_adapter.go
+++ internal/infra/queue/memory_queue_adapter.go
@@ -1,27 +1,61 @@
 package queue
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"log"
+	"net/http"
 	"reflect"
 )
 
 type MemoryQueueAdapter struct {
+	listeners map[string][]Listener
 }
 
 func NewMemoryQueueAdapter() *MemoryQueueAdapter {
-	return &MemoryQueueAdapter{}
+	return &MemoryQueueAdapter{
+		listeners: make(map[string][]Listener),
+	}
 }
 
-func (m MemoryQueueAdapter) Publish(ctx context.Context, eventPayload interface{}) error {
-	eventType := reflect.TypeOf(eventPayload)
+func (eb *MemoryQueueAdapter) ListenerRegister(eventType reflect.Type, handler func(w http.ResponseWriter, r *http.Request)) {
+	eb.listeners[eventType.Name()] = append(eb.listeners[eventType.Name()], Listener{eventType: eventType, callback: handler})
+}
 
-	payloadJson, err := json.Marshal(eventPayload)
-	if err != nil {
-		return err
+func (eb MemoryQueueAdapter) Publish(ctx context.Context, eventPayload interface{}) error {
+	eventType := reflect.TypeOf(eventPayload)
+	payloadJson, _ := json.Marshal(eventPayload)
+	
+	log.Printf("--- Publish %s ---", eventType)
+
+	for _, listener := range eb.listeners[eventType.Name()] {
+		w := NewQueueResponseWriter()
+		body := bytes.NewBuffer(payloadJson)
+
+		r, err := http.NewRequestWithContext(ctx, http.MethodPost, eventType.Name(), body)
+
+		if err != nil {
+			return err
+		}
+
+		listener.callback(w, r)
 	}
 
-	log.Printf("** [Publish] %s: %v ---", eventType, string(payloadJson))
+	return nil
+}
+
+func (eb *MemoryQueueAdapter) Connect(ctx context.Context) error {
+	log.Println("--- MemoeyQueueAdapter connected ---")
+	return nil
+}
+
+func (eb *MemoryQueueAdapter) Disconnect(ctx context.Context) error {
+	log.Println("--- MemoeyQueueAdapter disconnected ---")
+	return nil
+}
+
+func (eb *MemoryQueueAdapter) StartConsuming(ctx context.Context, queueName string) error {
+	log.Printf("--- MemoeyQueueAdapter StartConsuming queue %s ---", queueName)
 	return nil
 }
